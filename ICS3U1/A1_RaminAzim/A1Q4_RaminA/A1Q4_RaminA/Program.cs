23bed2b3079f8413c034bb1c10351d727404e328
Ramin Azim <127709108+raminazim@users.noreply.github.com>
2025-02-12 15:46:26 -0500

    commit

---
+++ ICS3U1/A1_RaminAzim/A1Q4_RaminA/A1Q4_RaminA/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Name: Ramin
+ * Date:02/02/25
+ * Title: Donut
+ * Purpose: Display "Amen" by Drake in the shape of a donut
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1Q4_RaminA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("AMEN");
+            Console.WriteLine("By Drake \n");
+            Console.WriteLine("                      to say thank You That's                      ");
+            Console.WriteLine("                 all, I don't wanna ask for anything,                 ");
+            Console.WriteLine("               I just wanna say thank You, Lord Thank              ");
+            Console.WriteLine("            You Prayin', prayin' (thank You) Pray again,             ");
+            Console.WriteLine("           pray again (Thank You, Father) Prayin \' (Thank          ");
+            Console.WriteLine("         You, Lord) Prayin' 'til you find a man That's gon'         ");
+            Console.WriteLine("       treat you That's gon' treat you like I can (amen, amen       ");
+            Console.WriteLine("    ) That's gon' please you That's gon' please you like I can     ");
+            Console.WriteLine("    (church) Church like a priest, Turks for the week Purse for     ");
+            Console.WriteLine("   yourself, purse for your niece Know you don't do Percs, baby,   ");
+            Console.WriteLine("   but (take your time) That's the perks of datin' me Red Merces   ");
+            Console.WriteLine("with the red seats Buy red wig, part the Red Sea With a neat freak ");
+            Console.WriteLine(" gettin' mess- God, forgive me          than your father ever sent ");
+            Console.WriteLine(" Spent more than your                    father did And you my baby");
+            Console.WriteLine("so I gotta put you in                       crib Same neighborhood ");
+            Console.WriteLine("where Ashton Kutcher                        I'm just doin' what that ");
+            Console.WriteLine("punk should have did                         You Lord) She prayin' for");
+            Console.WriteLine("me while I'm on the                         Prayin' for me while I");
+            Console.WriteLine("hold her close Prayin'                     that there's not no other ");
+            Console.WriteLine("girl I'm prayin' that                     these girls'll never know ");
+            Console.WriteLine("She prayin', asking God                  for a sign I just went and");
+            Console.WriteLine("paid her car note (note,                , note...) Turn to your n ");
+            Console.WriteLine(" eighbors And say \"Neighbor\"          Prayin', prayin' (neighbor) ");
+            Console.WriteLine(" Pray again, pray again (everybody, say it) Prayin' Prayin' 't  ");
+            Console.WriteLine("   you find a man That's gon' treat you That's gon' treat you ");
+            Console.WriteLine("    like I can That's gon' please you That's gon' please you    ");
+            Console.WriteLine("        like I can Thank you for calling Mercedes Benz in     ");
+            Console.WriteLine("           Beaumont If you know your party's extension     ");
+            Console.WriteLine("                you may dial it at any time or dial    ");
+            Console.WriteLine("                   \"9\" for company directory for ");
+            Console.WriteLine("                     company directory press 1                     ");
+            Console.WriteLine("Ramin Azim");
+
+            Console.ReadKey();
+        }
+    }
+}
