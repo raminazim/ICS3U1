bd92b710c3af2f77d5390378ca66fd12a4cec84e
Ramin Azim <127709108+raminazim@users.noreply.github.com>
2025-02-12 15:47:28 -0500

    Commit

--- A1_RaminAzim/A1Q4_RaminA/A1Q4_RaminA/Program.cs
+++ A1_RaminAzim/A1Q4_RaminA/A1Q4_RaminA/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Name: Ramin
+ * Date:02/02/25
+ * Title: Donut
+ * Purpose: Display "Amen" by Drake in the shape of a donut
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,59 +16,40 @@ namespace A1Q4_RaminA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n\n\n\n");
+
             Console.WriteLine("AMEN");
-            Console.WriteLine("Drake\n");
-            Console.WriteLine("                                                                     Father God, I come to You to say");
-            Console.WriteLine("                                                                 thank You, that's       all, I don't wanna");
-            Console.WriteLine("                                                             ask for anything,                I just wanna say");
-            Console.WriteLine("                                                         thank You, Lord,                                 Thank You\n");
-            Console.WriteLine("                                                     Prayin', prayin'                                         (thank You)");
-            Console.WriteLine("                                             Pray again, pray                                             again (thank You)");
-            Console.WriteLine("                                             Prayin' (                                                       thank You, Lord)");
-            Console.WriteLine("                                         Prayin' 'til                                                            you find a man\n");
-            Console.WriteLine("                                     That's gon' treat                                                                you like I can");
-            Console.WriteLine("                                 That's gon' please                                                                       you like I can");
-            Console.WriteLine("                             Church like                                                                                      a priest, Turks");
-            Console.WriteLine("                         for the week,                                                                                        Purse for yourself");
-            Console.WriteLine("                     purse for your                                                                                                  niece, Know you");
-            Console.WriteLine("                 don't do Percs                                                                                                          , baby, but\n");
-            Console.WriteLine("             That's the                                                                                                                 perks of datin' me");
-            Console.WriteLine("         Red Merces with                                                                                                                         the red seats");
-            Console.WriteLine("         Buy red wig,                                                                                                                         part the Red Sea");
-            Console.WriteLine("     With a neat                                                                                                                             freak, gettin' mess-");
-            Console.WriteLine("     God, forgive                                                                                                                              me, Father, I've\n");
-            Console.WriteLine("     sinned, Sent                                                                                                                                     more than");
-            Console.WriteLine(" your father                                                                                                                                              ever sent");
-            Console.WriteLine(" Spent more                                                                                                                                          than your baby");
-            Console.WriteLine(" father did,                                                                                                                                       And you my baby,");
-            Console.WriteLine(" so I gotta                                                                                                                                              put you in\n");
-            Console.WriteLine(" the crib,                                                                                                                                                    Same");
-            Console.WriteLine("neighborhood                                                                                                                                               where Ashton");
-            Console.WriteLine(" Kutcher live, I'm                                                                                                                                    just doin' what");
-            Console.WriteLine(" that punk should                                                                                                                                         have did\n");
-            Console.WriteLine(" She prayin'                                                                                                                                          for me while");
-            Console.WriteLine(" I'm on the                                                                                                                                          road, Prayin'");
-            Console.WriteLine(" for me while I                                                                                                                                  hold her close");
-            Console.WriteLine("     Prayin' that                                                                                                                              there's not no\n");
-            Console.WriteLine("     other girl,                                                                                                                                  I'm prayin'");
-            Console.WriteLine("     that these                                                                                                                              girls'll never");
-            Console.WriteLine("         know, She prayin',                                                                                                                   asking God");
-            Console.WriteLine("         for a sign, I                                                                                                                   just went and\n");
-            Console.WriteLine("             paid her car                                                                                                            note (note, note)");
-            Console.WriteLine("                 Turn to your                                                                                                      neighbors and say");
-            Console.WriteLine("                     \"Neighbor\", Prayin',                                                                                     prayin' (neighbor)");
-            Console.WriteLine("                         Pray again, pray                                                                                      again (everybody)");
-            Console.WriteLine("                             Prayin' 'til you                                                                                     find a man\n");
-            Console.WriteLine("                                 That's gon' treat                                                                           you like I can");
-            Console.WriteLine("                                     That's gon' please                                                                   you like I can\n");
-            Console.WriteLine("                                         Thank you for                                                                 calling Mercedes");
-            Console.WriteLine("                                             Benz in Beaumont,                                                            If you know");
-            Console.WriteLine("                                                 your party's                                                        extension, you");
-            Console.WriteLine("                                                     may dial it                                                     at any time");
-            Console.WriteLine("                                                         Or dial '9'                                                for company");
-            Console.WriteLine("                                                                   directory, For                     sales, please");
-            Console.WriteLine("                                                                                 press '1'\n");
+            Console.WriteLine("By Drake \n");
+            Console.WriteLine("                      to say thank You That's                      ");
+            Console.WriteLine("                 all, I don't wanna ask for anything,                 ");
+            Console.WriteLine("               I just wanna say thank You, Lord Thank              ");
+            Console.WriteLine("            You Prayin', prayin' (thank You) Pray again,             ");
+            Console.WriteLine("           pray again (Thank You, Father) Prayin \' (Thank          ");
+            Console.WriteLine("         You, Lord) Prayin' 'til you find a man That's gon'         ");
+            Console.WriteLine("       treat you That's gon' treat you like I can (amen, amen       ");
+            Console.WriteLine("    ) That's gon' please you That's gon' please you like I can     ");
+            Console.WriteLine("    (church) Church like a priest, Turks for the week Purse for     ");
+            Console.WriteLine("   yourself, purse for your niece Know you don't do Percs, baby,   ");
+            Console.WriteLine("   but (take your time) That's the perks of datin' me Red Merces   ");
+            Console.WriteLine("with the red seats Buy red wig, part the Red Sea With a neat freak ");
+            Console.WriteLine(" gettin' mess- God, forgive me          than your father ever sent ");
+            Console.WriteLine(" Spent more than your                    father did And you my baby");
+            Console.WriteLine("so I gotta put you in                       crib Same neighborhood ");
+            Console.WriteLine("where Ashton Kutcher                        I'm just doin' what that ");
+            Console.WriteLine("punk should have did                         You Lord) She prayin' for");
+            Console.WriteLine("me while I'm on the                         Prayin' for me while I");
+            Console.WriteLine("hold her close Prayin'                     that there's not no other ");
+            Console.WriteLine("girl I'm prayin' that                     these girls'll never know ");
+            Console.WriteLine("She prayin', asking God                  for a sign I just went and");
+            Console.WriteLine("paid her car note (note,                , note...) Turn to your n ");
+            Console.WriteLine(" eighbors And say \"Neighbor\"          Prayin', prayin' (neighbor) ");
+            Console.WriteLine(" Pray again, pray again (everybody, say it) Prayin' Prayin' 't  ");
+            Console.WriteLine("   you find a man That's gon' treat you That's gon' treat you ");
+            Console.WriteLine("    like I can That's gon' please you That's gon' please you    ");
+            Console.WriteLine("        like I can Thank you for calling Mercedes Benz in     ");
+            Console.WriteLine("           Beaumont If you know your party's extension     ");
+            Console.WriteLine("                you may dial it at any time or dial    ");
+            Console.WriteLine("                   \"9\" for company directory for ");
+            Console.WriteLine("                     company directory press 1                     ");
             Console.WriteLine("Ramin Azim");
 
             Console.ReadKey();
