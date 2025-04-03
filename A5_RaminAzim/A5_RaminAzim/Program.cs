727fd02f950d198af59eca28a453f504b679e90c
Ramin Azim <127709108+raminazim@users.noreply.github.com>
2025-04-03 10:37:48 -0400

    Update Program.cs

--- A5_RaminAzim/A5_RaminAzim/Program.cs
+++ A5_RaminAzim/A5_RaminAzim/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,92 +10,79 @@ namespace A5_RaminAzim
     {
         static void Main(string[] args)
         {
-            //variable decloration
-            int intSelect;
-
             //menu
             while (true)
             {
-                Console.ForegroundColor = ConsoleColor.White;//in number 4 it would change color so this stops it
-                Console.WriteLine("______________________________");
-                Console.WriteLine("|------------Menu------------|");
-                Console.WriteLine("|Number display =========> 1 |");
-                Console.WriteLine("|Encrypt Number =========> 2 |");
-                Console.WriteLine("|Number display 2 =======> 3 |");
-                Console.WriteLine("|Back word rainbow ======> 4 |");
-                Console.WriteLine("|name pattern ===========> 5 |");
-                Console.WriteLine("|123 pattern ============> 6 |");
-                Console.WriteLine("|Exit ===================> 7 |");
-                Console.WriteLine("******************************");
-                Console.WriteLine();//spacer
-                                    //input
-                Console.Write("Which program would you like? Enter:");
-                intSelect = int.Parse(Console.ReadLine());
-                //process and output
-                if (intSelect == 1)
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("ASSIGNMENT 5");
+
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Menu(); //Display menu
+                string strUsr;
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Choose Option: ");
+                strUsr = Console.ReadLine();
+
+                //Execute corresponding function based on user input
+                if (strUsr == "1")
                 {
-                    Q1NumDis();
+                    Q1_NumDis();
                 }
-                else if (intSelect == 2)
+                else if (strUsr == "2")
                 {
-                    Q2Encrypt();
+                    Q2_Encrypt();
                 }
-                else if (intSelect == 3)
+                else if (strUsr == "3")
                 {
-                    Q3NumDis2();
+                    Q3_NumDis2();
                 }
-                else if (intSelect == 4)
+                else if (strUsr == "4")
                 {
-                    Q4BackRain();
+                    Q4_BackRain();
                 }
-                else if (intSelect == 5)
+                else if (strUsr == "5")
                 {
-                    Q5Pattern();
+                    Q5_Pattern();
                 }
-                else if (intSelect == 6)
+                else if (strUsr == "6")
                 {
-                    Q6123();
-                }              
-                else if (intSelect == 7)
-                {
-                    Console.WriteLine("Come Again!");
-                    break;
+                    Q6_123();
                 }
                 else
                 {
-                    Console.WriteLine("Invalid Input");
+                    Environment.Exit(0); //Exit the program
                 }
-                Console.ReadKey();
-                Console.Clear();
             }
-
             Console.ReadKey();
-            //Q1NumDis();
-            //Q2Encrypt();
-            //Q3NumDis2();
-            //Q4BackRain();
-            //Q5Pattern();
-            //Q6123();
-
         }
-        public static void Q1NumDis()
+        
+        public static void Menu()
         {
-            //variable decloration
-            string strNum, strUI;
+            Console.WriteLine("1 - Number Display");
+            Console.WriteLine("2 - Encrypt");
+            Console.WriteLine("3 -  Number Display 2");
+            Console.WriteLine("4 - Backwards Rainbow");
+            Console.WriteLine("5 - Pattern");
+            Console.WriteLine("6 - 123 Pattern");
+            Console.WriteLine("Any Other Input - Exit");
+        }
+        public static void Q1_NumDis()
+        {
+            //Declaration
+            string strUsr, strNum;
             int intNum;
 
-            Console.WriteLine("Enter number: "); //input
-            intNum = int.Parse(strUI = Console.ReadLine()); //parse in one line
+            //Input
+            Console.Write("Enter number: ");
+            intNum = int.Parse(strUsr = Console.ReadLine());
 
-           
-            strNum = intNum.ToString(); //make a string
-           
-            //do until end of number
+
+            strNum = intNum.ToString();
+
+            //Process/Output
             for (int i = 0; i < strNum.Length; i++)
-            {
-
-
-                //process and output
+            { 
                 if (strNum[i] == '0')
                 {
                     Console.Write("zero ");
@@ -136,81 +123,81 @@ namespace A5_RaminAzim
                 {
                     Console.Write("nine ");
                 }
-               
+
             }
-
+            Console.WriteLine();
         }
-        public static void Q2Encrypt()
+        public static void Q2_Encrypt()
         {
-            //variable decloration
-            string strUI, strNum, strFirst, strLast, strMiddle, strMiddle2;
-            int intNum, intFirst, intMiddle, intMiddle2, intLast;
+            //Declaration
+            string strUsr, strNum, strDigOne, strMiddle, strDigThree, strDigFour;
+            int intNum, intDigOne, intDigTwo, intDigThree, intDigFour;
 
-            Console.WriteLine("Enter a 4 digit number: ");
+            //Input
+            Console.Write("4 Dig Num: ");
             strNum = Console.ReadLine();
 
- 
-            //process
-            strFirst = strNum.Substring(3);//take last number and make it first
+
+            //Process
+            strDigOne = strNum.Substring(3);
+            strDigFour = strNum.Substring(0, 1);
+            strMiddle = strNum.Substring(1, 2);
+            strDigThree = strNum.Substring(2);
 
 
-            strLast = strNum.Substring(0, 1); //first to last
- 
+            intDigOne = int.Parse(strDigOne);
+            intDigTwo = int.Parse(strMiddle);
+            intDigThree = int.Parse(strDigThree);
+            intDigFour = int.Parse(strDigFour);
 
-            strMiddle = strNum.Substring(1, 2); //middle 2 numbers
+            intDigOne = intDigOne * 1000;
+            intDigTwo = intDigTwo * 10;
+            intDigThree = intDigThree ^ (intDigOne / 1000);
+            intNum = intDigOne + intDigTwo + intDigFour + intDigThree;
 
-
-            strMiddle2 = strNum.Substring(2); //just 3rd digit
-
-            //parse
-            intFirst = int.Parse(strFirst);
-            intMiddle = int.Parse(strMiddle);
-            intMiddle2 = int.Parse(strMiddle2);
-            intLast = int.Parse(strLast);
-            //make it one number
-            intFirst = intFirst * 1000;
-            intMiddle = intMiddle * 10;
-            intMiddle2 = intMiddle2 ^ (intFirst / 1000);
-            intNum = intFirst + intMiddle + intLast + intMiddle2;
-
-            intNum = intNum / 5;//encrpyts
+            intNum = intNum / 5;
             intNum = intNum + (intNum * 5);
 
-            if (intNum > 9999)//make it 4 digits if its greater than 4 digits
+            if (intNum > 9999)
             {
                 intNum = intNum / 10;
             }
             //output
-            Console.WriteLine("Encrypted number is: " + intNum);
+            Console.WriteLine("Encrypted Number: " + intNum);
+            Console.WriteLine();
             Console.WriteLine("Encryption Process: ");
-            Console.WriteLine("1. Swap first and last numbers ");
-            Console.WriteLine("2. Add the third digit to the power of the first digit to the total ");
-            Console.WriteLine("3. divide total by 5 ");
-            Console.WriteLine("4. add total from before division to divided number ");
+            Console.WriteLine("1. Swap 1st 4th Digit");
+            Console.WriteLine("2. Add 3rd Digit To Power Of 1st Digit To Total ");
+            Console.WriteLine("3. Divide Total By 5");
+            Console.WriteLine("4. Add Total From Previous Division To Divided Number");
 
+            Console.WriteLine();
         }
-        public static void Q3NumDis2()
+        public static void Q3_NumDis2()
         {
-            //variable decloration
-            string strUI, strNum, strHun, strTen, strOne;
-            int intNum , intHun, intTen, intOne, intTeen;
-            //input
-            Console.WriteLine("Enter a Number: ");
+            //Declaration
+            string strUsr, strNum, strHun, strTen, strOne;
+            int intNum, intHun, intTen, intOne, intTeen;
+
+            //Input
+            Console.WriteLine("Enter a Number (>1000): ");
             intNum = Int32.Parse(Console.ReadLine());
-            //process
-            intHun = intNum / 100; //hundred place
-            intTen = (intNum - (intHun*100)) / 10; //tens place
-            intOne = intNum - (intHun * 100) - (intTen*10); //ones place
-            intTeen = (intTen * 10) + intOne; //teens case
-            //process and output
+
+            //Process
+            intHun = intNum / 100;
+            intTen = (intNum - (intHun * 100)) / 10;
+            intOne = intNum - (intHun * 100) - (intTen * 10);
+            intTeen = (intTen * 10) + intOne;
+            
+            //Process/Output
             while (true)
             {
                 if (intNum == 1000)
                 {
-                    Console.WriteLine("One Thousand");
+                    Console.Write("One Thousand");
                     break;
                 }
-                //hundreds
+
                 if (intHun == 1)
                 {
                     Console.Write("One Hundred ");
@@ -247,10 +234,10 @@ namespace A5_RaminAzim
                 {
                     Console.Write("Nine Hundred ");
                 }
-                //teens
+                
+                //Teens
                 if (10 < intTeen && intTeen < 20)
                 {
-                    //teens //if teen display break so ten and ones dont display
                     if (intTeen == 11)
                     {
                         Console.Write("Eleven ");
@@ -289,7 +276,7 @@ namespace A5_RaminAzim
                     }
                     break;
                 }
-                //tens
+
                 if (intTen == 1)
                 {
                     Console.Write("Ten ");
@@ -326,7 +313,7 @@ namespace A5_RaminAzim
                 {
                     Console.Write("Ninety ");
                 }
-                //ones
+
                 if (intOne == 1)
                 {
                     Console.Write("One ");
@@ -365,27 +352,29 @@ namespace A5_RaminAzim
                 }
                 break;
             }
+
+            Console.WriteLine();
         }
-        public static void Q4BackRain()
+        public static void Q4_BackRain()
         {
-            //variable decloration
+            //Declaration
             string strSentence;
-            int intCount=0;
-            //input
-            Console.WriteLine("Enter a sentence: ");
+            int intCount = 0;
+
+            //Input
+            Console.WriteLine("Sentence: ");
             strSentence = Console.ReadLine();
-            //process and output
+
+            //Process/Output
             for (int i = strSentence.Length - 1; i >= 0; i--)
-            {
-               
-                //rainbow colors
+            { 
                 if (intCount == 0)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                 }
                 else if (intCount == 1)
                 {
-                    Console.ForegroundColor= ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                 }
                 else if (intCount == 2)
                 {
@@ -404,46 +393,53 @@ namespace A5_RaminAzim
                     Console.ForegroundColor = ConsoleColor.Magenta;
                 }
                 intCount++;
-                Console.Write(strSentence.Substring(i, 1)); //backword display
-                if (intCount > 5) //rainbow reset
+                Console.Write(strSentence.Substring(i, 1));
+                if (intCount > 5)
                 {
                     intCount = 0;
                 }
             }
 
+            Console.WriteLine();
         }
-        public static void Q5Pattern()
+        public static void Q5_Pattern()
         {
-            //variable decloration
-            string strName = "Zachary Tozman";
-           
-            int intSize = 9;//number of rows
-            //process and output
+            //Declaration
+            string strName = "Ramin Azim";
+            int intSize = 9;
+
+            //No Input
+
+            //Process/Output
             for (int i = 0; i < intSize; i++)
             {
                 for (int x = 0; x < intSize; x++)
                 {
-                   
+
                     if (x == i || x == intSize - i - 1)
                     {
                         Console.Write(strName + " ");
                     }
                     else
                     {
-                        Console.Write("     ");//space between name
+                        Console.Write("     ");
                     }
                 }
                 Console.WriteLine();
             }
+
+            Console.WriteLine();
         }
-        public static void Q6123()
+        public static void Q6_123()
         {
-            //variable decloration
+            //Declaration
             string strSentence;
-            //input
+
+            //Input
             Console.WriteLine("Enter a sentence: ");
             strSentence = Console.ReadLine();
-            //process and output
+
+            //Process/Output
             for (int i = 0; i < strSentence.Length;)
             {
                 if (i + 1 > strSentence.Length)
@@ -451,7 +447,7 @@ namespace A5_RaminAzim
                     break;
                 }
 
-                Console.WriteLine(strSentence.Substring(i, 1));//first in pattern 1
+                Console.WriteLine(strSentence.Substring(i, 1));
 
                 i++;
 
@@ -460,20 +456,22 @@ namespace A5_RaminAzim
                     break;
                 }
 
-                Console.WriteLine(strSentence.Substring(i, 2)); //2 characters
+                Console.WriteLine(strSentence.Substring(i, 2));
 
-                i+=2;
-               
+                i += 2;
+
                 if (i + 3 > strSentence.Length)
                 {
                     break;
                 }
 
-                Console.WriteLine(strSentence.Substring(i, 3));//3 characters
+                Console.WriteLine(strSentence.Substring(i, 3));
 
-                i+=3;
+                i += 3;
 
             }
+
+            Console.WriteLine();
         }
     }
 }
