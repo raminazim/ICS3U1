533240312096a4cc157ee909ddada773af8dbec6
Ramin Azim <127709108+raminazim@users.noreply.github.com>
2025-04-03 22:29:09 -0400

    Update Program.cs

--- A5_RaminAzim/A5_RaminAzim/Program.cs
+++ A5_RaminAzim/A5_RaminAzim/Program.cs
@@ -1,13 +1,10 @@
-﻿/* Name: Ramin Azim
+/* Name: Ramin Azim
  * Date: 04/01/25
  * Title: Assignment 5
- *  Purpose: Menu driven program for questions given
+ * Purpose: To create a program that uses a menu and does different questions
  */
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace A5_RaminAzim
 {
@@ -15,7 +12,7 @@ namespace A5_RaminAzim
     {
         static void Main(string[] args)
         {
-            //menu
+            //Ask user which option they want to pick (keep looping)
             while (true)
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
@@ -23,458 +20,208 @@ namespace A5_RaminAzim
 
                 Console.ForegroundColor = ConsoleColor.DarkBlue;
                 Menu(); //Display menu
-                string strUsr;
 
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("Choose Option: ");
-                strUsr = Console.ReadLine();
+                string strUsr = Console.ReadLine();
 
-                //Execute corresponding function based on user input
-                if (strUsr == "1")
-                {
-                    Q1_NumDis();
-                }
-                else if (strUsr == "2")
-                {
-                    Q2_Encrypt();
-                }
-                else if (strUsr == "3")
-                {
-                    Q3_NumDis2();
-                }
-                else if (strUsr == "4")
-                {
-                    Q4_BackRain();
-                }
-                else if (strUsr == "5")
-                {
-                    Q5_Pattern();
-                }
-                else if (strUsr == "6")
-                {
-                    Q6_123();
-                }
-                else
-                {
-                    Environment.Exit(0); //Exit the program
-                }
+                // Call the corresponding method depending on the user's input
+                if (strUsr == "1") Q1_NumDis();
+                else if (strUsr == "2") Q2_Encrypt();
+                else if (strUsr == "3") Q3_NumDis2();
+                else if (strUsr == "4") Q4_BackRain();
+                else if (strUsr == "5") Q5_Pattern();
+                else if (strUsr == "6") Q6_123();
+                else Environment.Exit(0); // Exit if input is anything else
             }
-            Console.ReadKey();
         }
-        
+
         public static void Menu()
         {
+            // Display the menu
             Console.WriteLine("1 - Number Display");
             Console.WriteLine("2 - Encrypt");
-            Console.WriteLine("3 -  Number Display 2");
+            Console.WriteLine("3 - Number Display 2");
             Console.WriteLine("4 - Backwards Rainbow");
             Console.WriteLine("5 - Pattern");
             Console.WriteLine("6 - 123 Pattern");
             Console.WriteLine("Any Other Input - Exit");
         }
+
         public static void Q1_NumDis()
         {
             //Declaration
-            string strUsr, strNum;
-            int intNum;
+            string strNum;
 
             //Input
             Console.Write("Enter number: ");
-            intNum = int.Parse(strUsr = Console.ReadLine());
-
-
-            strNum = intNum.ToString();
+            int intNum = int.Parse(Console.ReadLine());
 
             //Process/Output
-            for (int i = 0; i < strNum.Length; i++)
-            { 
-                if (strNum[i] == '0')
-                {
-                    Console.Write("zero ");
-                }
-                else if (strNum[i] == '1')
-                {
-                    Console.Write("one ");
-                }
-                else if (strNum[i] == '2')
-                {
-                    Console.Write("two ");
-                }
-                else if (strNum[i] == '3')
-                {
-                    Console.Write("three ");
-                }
-                else if (strNum[i] == '4')
-                {
-                    Console.Write("four ");
-                }
-                else if (strNum[i] == '5')
-                {
-                    Console.Write("five ");
-                }
-                else if (strNum[i] == '6')
-                {
-                    Console.Write("six ");
-                }
-                else if (strNum[i] == '7')
-                {
-                    Console.Write("seven ");
-                }
-                else if (strNum[i] == '8')
-                {
-                    Console.Write("eight ");
-                }
-                else if (strNum[i] == '9')
-                {
-                    Console.Write("nine ");
-                }
+            strNum = intNum.ToString(); // Convert number to string so we can loop through digits
 
+            for (int i = 0; i < strNum.Length; i++)
+            {
+                // Check each digit and print its word version
+                if (strNum[i] == '0') Console.Write("zero ");
+                else if (strNum[i] == '1') Console.Write("one ");
+                else if (strNum[i] == '2') Console.Write("two ");
+                else if (strNum[i] == '3') Console.Write("three ");
+                else if (strNum[i] == '4') Console.Write("four ");
+                else if (strNum[i] == '5') Console.Write("five ");
+                else if (strNum[i] == '6') Console.Write("six ");
+                else if (strNum[i] == '7') Console.Write("seven ");
+                else if (strNum[i] == '8') Console.Write("eight ");
+                else if (strNum[i] == '9') Console.Write("nine ");
             }
+
             Console.WriteLine();
         }
+
         public static void Q2_Encrypt()
         {
             //Declaration
-            string strUsr, strNum, strDigOne, strMiddle, strDigThree, strDigFour;
-            int intNum, intDigOne, intDigTwo, intDigThree, intDigFour;
+            string strNum, d1, d4, mid, d3;
+            int i1, i2, i3, i4, intNum;
 
             //Input
             Console.Write("4 Dig Num: ");
             strNum = Console.ReadLine();
 
-
             //Process
-            strDigOne = strNum.Substring(3);
-            strDigFour = strNum.Substring(0, 1);
-            strMiddle = strNum.Substring(1, 2);
-            strDigThree = strNum.Substring(2);
+            d1 = strNum.Substring(3); // 4th digit
+            d4 = strNum.Substring(0, 1); // 1st digit
+            mid = strNum.Substring(1, 2); // 2nd and 3rd
+            d3 = strNum.Substring(2); // 3rd digit
 
+            // Convert all to integers
+            i1 = int.Parse(d1);
+            i2 = int.Parse(mid);
+            i3 = int.Parse(d3);
+            i4 = int.Parse(d4);
 
-            intDigOne = int.Parse(strDigOne);
-            intDigTwo = int.Parse(strMiddle);
-            intDigThree = int.Parse(strDigThree);
-            intDigFour = int.Parse(strDigFour);
+            i1 *= 1000; // Shift 4th digit to 1000s place
+            i2 *= 10; // Move 2nd+3rd to tens place
+            i3 ^= (i1 / 1000); // XOR 3rd digit with original 4th
+            intNum = i1 + i2 + i3 + i4; // Add all parts
+            intNum /= 5; // Divide by 5
+            intNum += intNum * 5; // Multiply result and add
 
-            intDigOne = intDigOne * 1000;
-            intDigTwo = intDigTwo * 10;
-            intDigThree = intDigThree ^ (intDigOne / 1000);
-            intNum = intDigOne + intDigTwo + intDigFour + intDigThree;
+            if (intNum > 9999) intNum /= 10; // If too long, fix length
 
-            intNum = intNum / 5;
-            intNum = intNum + (intNum * 5);
-
-            if (intNum > 9999)
-            {
-                intNum = intNum / 10;
-            }
-            //output
+            //Output
             Console.WriteLine("Encrypted Number: " + intNum);
             Console.WriteLine();
-            Console.WriteLine("Encryption Process: ");
+            Console.WriteLine("Encryption Process:");
             Console.WriteLine("1. Swap 1st 4th Digit");
-            Console.WriteLine("2. Add 3rd Digit To Power Of 1st Digit To Total ");
-            Console.WriteLine("3. Divide Total By 5");
-            Console.WriteLine("4. Add Total From Previous Division To Divided Number");
-
+            Console.WriteLine("2. 3rd Digit ^ 1st Digit");
+            Console.WriteLine("3. ÷5");
+            Console.WriteLine("4. Add previous result");
             Console.WriteLine();
         }
+
         public static void Q3_NumDis2()
         {
             //Declaration
-            string strUsr, strNum, strHun, strTen, strOne;
-            int intNum, intHun, intTen, intOne, intTeen;
+            int intNum, h, t, o, teen;
+            string[] hundreds = { "", "One Hundred", "Two Hundred", "Three Hundred", "Four Hundred", "Five Hundred", "Six Hundred", "Seven Hundred", "Eight Hundred", "Nine Hundred" };
+            string[] tens = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+            string[] ones = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+            string[] teens = { "", "", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Six Teen", "Seven Teen", "Eight Teen", "Nine Teen" };
 
             //Input
             Console.WriteLine("Enter a Number (>1000): ");
-            intNum = Int32.Parse(Console.ReadLine());
+            intNum = int.Parse(Console.ReadLine());
 
-            //Process
-            intHun = intNum / 100;
-            intTen = (intNum - (intHun * 100)) / 10;
-            intOne = intNum - (intHun * 100) - (intTen * 10);
-            intTeen = (intTen * 10) + intOne;
-            
             //Process/Output
-            while (true)
+            if (intNum == 1000)
             {
-                if (intNum == 1000)
-                {
-                    Console.Write("One Thousand");
-                    break;
-                }
-
-                if (intHun == 1)
-                {
-                    Console.Write("One Hundred ");
-                }
-                else if (intHun == 2)
-                {
-                    Console.Write("Two Hundred ");
-                }
-                else if (intHun == 3)
-                {
-                    Console.Write("Three Hundred ");
-                }
-                else if (intHun == 4)
-                {
-                    Console.Write("Four Hundred ");
-                }
-                else if (intHun == 5)
-                {
-                    Console.Write("Five Hundred ");
-                }
-                else if (intHun == 6)
-                {
-                    Console.Write("Six hundred ");
-                }
-                else if (intHun == 7)
-                {
-                    Console.Write("Seven Hundred ");
-                }
-                else if (intHun == 8)
-                {
-                    Console.Write("Eight Hundred ");
-                }
-                else if (intHun == 9)
-                {
-                    Console.Write("Nine Hundred ");
-                }
-                
-                //Teens
-                if (10 < intTeen && intTeen < 20)
-                {
-                    if (intTeen == 11)
-                    {
-                        Console.Write("Eleven ");
-                    }
-                    else if (intTeen == 12)
-                    {
-                        Console.Write("Twelve ");
-                    }
-                    else if (intTeen == 13)
-                    {
-                        Console.Write("Thirteen ");
-                    }
-                    else if (intTeen == 14)
-                    {
-                        Console.Write("Fourteen ");
-                    }
-                    else if (intTeen == 15)
-                    {
-                        Console.Write("Fifteen ");
-                    }
-                    else if (intTeen == 16)
-                    {
-                        Console.Write("Six Teen");
-                    }
-                    else if (intTeen == 17)
-                    {
-                        Console.Write("Seven Teen");
-                    }
-                    else if (intTeen == 18)
-                    {
-                        Console.Write("Eight Teen ");
-                    }
-                    else if (intTeen == 19)
-                    {
-                        Console.Write("Nine Teen ");
-                    }
-                    break;
-                }
-
-                if (intTen == 1)
-                {
-                    Console.Write("Ten ");
-                }
-                else if (intTen == 2)
-                {
-                    Console.Write("Twenty ");
-                }
-                else if (intTen == 3)
-                {
-                    Console.Write("Thirty ");
-                }
-                else if (intTen == 4)
-                {
-                    Console.Write("Forty ");
-                }
-                else if (intTen == 5)
-                {
-                    Console.Write("Fifty ");
-                }
-                else if (intTen == 6)
-                {
-                    Console.Write("Sixty ");
-                }
-                else if (intTen == 7)
-                {
-                    Console.Write("Seventy ");
-                }
-                else if (intTen == 8)
-                {
-                    Console.Write("Eighty ");
-                }
-                else if (intTen == 9)
-                {
-                    Console.Write("Ninety ");
-                }
-
-                if (intOne == 1)
-                {
-                    Console.Write("One ");
-                }
-                else if (intOne == 2)
-                {
-                    Console.Write("Two ");
-                }
-                else if (intOne == 3)
-                {
-                    Console.Write("Three ");
-                }
-                else if (intOne == 4)
-                {
-                    Console.Write("Four ");
-                }
-                else if (intOne == 5)
-                {
-                    Console.Write("Five ");
-                }
-                else if (intOne == 6)
-                {
-                    Console.Write("Six ");
-                }
-                else if (intOne == 7)
-                {
-                    Console.Write("Seven ");
-                }
-                else if (intOne == 8)
-                {
-                    Console.Write("Eight ");
-                }
-                else if (intOne == 9)
-                {
-                    Console.Write("Nine ");
-                }
-                break;
+                Console.WriteLine("One Thousand");
+                return;
             }
 
+            h = intNum / 100; // Get hundreds digit
+            t = (intNum % 100) / 10; // Get tens digit
+            o = intNum % 10; // Get ones digit
+            teen = t * 10 + o; // Combine to check for teen
+
+            Console.Write(hundreds[h] + " ");
+
+            if (teen > 10 && teen < 20)
+            {
+                Console.WriteLine(teens[teen - 10]); // Handle teens separately
+                return;
+            }
+
+            Console.Write(tens[t] + " ");
+            Console.Write(ones[o]);
             Console.WriteLine();
         }
+
         public static void Q4_BackRain()
         {
             //Declaration
-            string strSentence;
-            int intCount = 0;
+            string s;
+            int c = 0;
 
             //Input
             Console.WriteLine("Sentence: ");
-            strSentence = Console.ReadLine();
+            s = Console.ReadLine();
 
             //Process/Output
-            for (int i = strSentence.Length - 1; i >= 0; i--)
-            { 
-                if (intCount == 0)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                }
-                else if (intCount == 1)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                }
-                else if (intCount == 2)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                }
-                else if (intCount == 3)
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                }
-                else if (intCount == 4)
-                {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                }
-                else if (intCount == 5)
-                {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                }
-                intCount++;
-                Console.Write(strSentence.Substring(i, 1));
-                if (intCount > 5)
-                {
-                    intCount = 0;
-                }
+            for (int i = s.Length - 1; i >= 0; i--) // Loop backwards
+            {
+                Console.ForegroundColor = (ConsoleColor)(c + 12); // Change color
+                Console.Write(s[i]);
+                c = (c + 1) % 6; // Cycle through 6 color offsets
             }
 
             Console.WriteLine();
         }
+
         public static void Q5_Pattern()
         {
             //Declaration
-            string strName = "Ramin Azim";
-            int intSize = 9;
-
-            //No Input
+            string name = "Ramin Azim";
+            int size = 9;
 
             //Process/Output
-            for (int i = 0; i < intSize; i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int x = 0; x < intSize; x++)
+                for (int x = 0; x < size; x++)
                 {
-
-                    if (x == i || x == intSize - i - 1)
-                    {
-                        Console.Write(strName + " ");
-                    }
-                    else
-                    {
-                        Console.Write("     ");
-                    }
+                    // Print name diagonally from both corners
+                    if (x == i || x == size - i - 1) Console.Write(name + " ");
+                    else Console.Write("     ");
                 }
                 Console.WriteLine();
             }
 
             Console.WriteLine();
         }
+
         public static void Q6_123()
         {
             //Declaration
-            string strSentence;
+            string s;
 
             //Input
             Console.WriteLine("Enter a sentence: ");
-            strSentence = Console.ReadLine();
+            s = Console.ReadLine();
 
             //Process/Output
-            for (int i = 0; i < strSentence.Length;)
+            for (int i = 0; i < s.Length;)
             {
-                if (i + 1 > strSentence.Length)
-                {
-                    break;
-                }
-
-                Console.WriteLine(strSentence.Substring(i, 1));
-
-                i++;
-
-                if (i + 2 > strSentence.Length)
-                {
-                    break;
-                }
-
-                Console.WriteLine(strSentence.Substring(i, 2));
-
+                // Print 1 char
+                if (i + 1 <= s.Length) Console.WriteLine(s.Substring(i++, 1));
+                // Print next 2 chars
+                if (i + 2 <= s.Length) Console.WriteLine(s.Substring(i, 2));
                 i += 2;
-
-                if (i + 3 > strSentence.Length)
-                {
-                    break;
-                }
-
-                Console.WriteLine(strSentence.Substring(i, 3));
-
+                // Print next 3 chars
+                if (i + 3 <= s.Length) Console.WriteLine(s.Substring(i, 3));
                 i += 3;
-
             }
+
             Console.WriteLine();
         }
     }
