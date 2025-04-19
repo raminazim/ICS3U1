2cdf7a619053011fe991b806c6dc2c7189c0f70a
Ramin Azim <127709108+raminazim@users.noreply.github.com>
2025-04-18 23:48:42 -0400

    Update Program.cs

--- A7_RaminAzim/A7_RaminAzim/Program.cs
+++ A7_RaminAzim/A7_RaminAzim/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -47,17 +47,27 @@ namespace A7_RaminAzim
         static string[] strFirstNameOG;
         static string[] strLastNameOG;
         static int[] intGradeOG;
+
+        static int intStudentCount;
         static void Main(string[] args)
         {
-            int intInputNum;
+            string strInput;
 
             //Asking For How Many Students For OG List
             Console.WriteLine("How Many Students Entering");
-            intInputNum = Int32.Parse(Console.ReadLine());
-            string strInput;
+            intStudentCount = Int32.Parse(Console.ReadLine());
+
+            //Getting Size
+            strFirstName = new string[intStudentCount];
+            strLastName = new string[intStudentCount];
+            intGrade = new int[intStudentCount];
+
+            strFirstNameOG = new string[intStudentCount];
+            strLastNameOG = new string[intStudentCount];
+            intGradeOG = new int[intStudentCount];
 
             //Getting OG List
-            for (int i = 0; i < intInputNum; i++)
+            for (int i = 0; i < intStudentCount; i++)
             {
                 Console.WriteLine("Enter First Name " + i + ": ");
                 strFirstName[i] = Console.ReadLine();
@@ -70,7 +80,7 @@ namespace A7_RaminAzim
             }
 
             //Copying OG List To Perma Copy
-            for (int i = 0; i < intInputNum; i++)
+            for (int i = 0; i < intStudentCount; i++)
             {
                 strFirstNameOG[i] = strFirstName[i];
 
@@ -78,24 +88,25 @@ namespace A7_RaminAzim
 
                 intGradeOG[i] = intGrade[i];
             }
-
-            //Asking User What To Do To List
-            Console.WriteLine("1: Show Original Unsorted List");
-            Console.WriteLine("2: Show Sorted List by Last Name(A-Z)");
-            Console.WriteLine("3: Show Sorted List by First Name(A-Z)");
-            Console.WriteLine("4: Show Sorted List by Grades(High - Low)");
-            Console.WriteLine("5: Delete a student");
-            Console.WriteLine("6: Add a student");
-            Console.WriteLine("7: Student Average for the glass");
-            Console.WriteLine("8: Student with Highest Grade");
-            Console.WriteLine("9: Student with Lowest Grade");
-            Console.WriteLine("10: Median Student(s) Grade");
-            Console.WriteLine("Any Other Input: Exit");
             
-            strInput = Console.ReadLine();
-
+            //Asking User What To Do To List
             while (true)
             {
+                Console.WriteLine("\n1: Show Original Unsorted List");
+                Console.WriteLine("2: Show Sorted List by Last Name(A-Z)");
+                Console.WriteLine("3: Show Sorted List by First Name(A-Z)");
+                Console.WriteLine("4: Show Sorted List by Grades(High - Low)");
+                Console.WriteLine("5: Delete a student");
+                Console.WriteLine("6: Add a student");
+                Console.WriteLine("7: Student Average for the glass");
+                Console.WriteLine("8: Student with Highest Grade");
+                Console.WriteLine("9: Student with Lowest Grade");
+                Console.WriteLine("10: Median Student(s) Grade");
+                Console.WriteLine("Any Other Input: Exit");
+
+                strInput = Console.ReadLine();
+
+            
                 if (strInput == "1")
                 {
                     OriginalList();
@@ -132,7 +143,7 @@ namespace A7_RaminAzim
                 {
                     GradeLow();
                 }
-                else if (strInput == "10")
+                else if (strInput == "10" || strInput == "0") //muscle memory rules 
                 {
                     Median();
                 }
@@ -144,7 +155,7 @@ namespace A7_RaminAzim
                 Console.ReadKey();
             }
         }
-        public static void OriginalList() 
+        public static void OriginalList()
         {
             Console.WriteLine("\nOriginal List:");
             Console.WriteLine("Last Name\tFirst Name\tGrade");
@@ -153,6 +164,8 @@ namespace A7_RaminAzim
             {
                 Console.WriteLine(strLastNameOG[i] + "\t\t" + strFirstNameOG[i] + "\t\t" + intGradeOG[i]);
             }
+
+            Console.WriteLine("\n Press Enter To Continue");
         }
         public static void SortLast()
         {
@@ -160,25 +173,23 @@ namespace A7_RaminAzim
             string tempLastName;
             int tempGrade;
 
-            // Bubble Sort by Last Name (A-Z)
             for (int i = 0; i < intStudentCount - 1; i++)
             {
                 for (int j = 0; j < intStudentCount - i - 1; j++)
                 {
-                    // Compare last names and swap if needed
+                    //Compares
                     if (String.Compare(strLastName[j], strLastName[j + 1]) > 0)
                     {
-                        // Swap last names
+                        //Swap
                         tempLastName = strLastName[j];
                         strLastName[j] = strLastName[j + 1];
                         strLastName[j + 1] = tempLastName;
 
-                        // Swap first names to maintain consistency
+                        //Swap others for consistency
                         tempFirstName = strFirstName[j];
                         strFirstName[j] = strFirstName[j + 1];
                         strFirstName[j + 1] = tempFirstName;
-    
-                        // Swap grades to maintain consistency
+
                         tempGrade = intGrade[j];
                         intGrade[j] = intGrade[j + 1];
                         intGrade[j + 1] = tempGrade;
@@ -186,19 +197,92 @@ namespace A7_RaminAzim
                 }
             }
 
-    // Display the sorted list
-            Console.WriteLine("\nSorted List by Last Name (A-Z):");
+            Console.WriteLine("\nSorted by Last Name):");
             Console.WriteLine("Last Name\tFirst Name\tGrade");
             for (int i = 0; i < intStudentCount; i++)
             {
                 Console.WriteLine(strLastName[i] + "\t\t" + strFirstName[i] + "\t\t" + intGrade[i]);
             }
+            
+            Console.WriteLine("\n Press Enter To Continue");
         }
         public static void SortFirst()
         {
+            string tempFirstName;
+            string tempLastName;
+            int tempGrade;
+
+            for (int i = 0; i < intStudentCount - 1; i++)
+            {
+                for (int j = 0; j < intStudentCount - i - 1; j++)
+                {
+                    //Compares
+                    if (String.Compare(strFirstName[j], strFirstName[j + 1]) > 0)
+                    {
+                        //Swap
+                        tempFirstName = strFirstName[j];
+                        strFirstName[j] = strFirstName[j + 1];
+                        strFirstName[j + 1] = tempFirstName;
+
+                        //Swap others for consistency
+                        tempLastName = strLastName[j];
+                        strLastName[j] = strLastName[j + 1];
+                        strLastName[j + 1] = tempLastName;
+
+                        tempGrade = intGrade[j];
+                        intGrade[j] = intGrade[j + 1];
+                        intGrade[j + 1] = tempGrade;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nSorted by First Name:");
+            Console.WriteLine("Last Name\tFirst Name\tGrade");
+            for (int i = 0; i < intStudentCount; i++)
+            {
+                Console.WriteLine(strLastName[i] + "\t\t" + strFirstName[i] + "\t\t" + intGrade[i]);
+            }
+
+            Console.WriteLine("\n Press Enter To Continue");
         }
         public static void SortGrade()
         {
+            string tempFirstName;
+            string tempLastName;
+            int tempGrade;
+
+            for (int i = 0; i < intStudentCount - 1; i++)
+            {
+                for (int j = 0; j < intStudentCount - i - 1; j++)
+                {
+                    //Compares
+                    if (intGrade[j].CompareTo(intGrade[j + 1]) < 0)
+                    {
+                        //Swap
+                        tempGrade = intGrade[j];
+                        intGrade[j] = intGrade[j + 1];
+                        intGrade[j + 1] = tempGrade;
+
+                        //Swap for consistency
+                        tempFirstName = strFirstName[j];
+                        strFirstName[j] = strFirstName[j + 1];
+                        strFirstName[j + 1] = tempFirstName;
+
+                        tempLastName = strLastName[j];
+                        strLastName[j] = strLastName[j + 1];
+                        strLastName[j + 1] = tempLastName;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nSorted by Grade: ");
+            Console.WriteLine("Last Name\tFirst Name\tGrade");
+            for (int i = 0; i < intStudentCount; i++)
+            {
+                Console.WriteLine(strLastName[i] + "\t\t" + strFirstName[i] + "\t\t" + intGrade[i]);
+            }
+
+            Console.WriteLine("\n Press Enter To Continue");
         }
         public static void Delete()
         {
@@ -209,15 +293,119 @@ namespace A7_RaminAzim
 
         public static void Avg()
         {
+            int intTotal = 0;
+            int intAvg = 0;
+
+            for (int i = 0;i < intStudentCount;i++)
+            {
+                intTotal = intTotal + intGrade[i];
+            }
+
+            intAvg = intTotal / intStudentCount;
+
+            Console.WriteLine("Average: " + intAvg);
+            
+            Console.WriteLine("\n Press Enter To Continue");
         }
         public static void GradeHigh()
         {
+            string tempFirstName;
+            string tempLastName;
+            int tempGrade;
+
+            for (int i = 0; i < intStudentCount - 1; i++)
+            {
+                for (int j = 0; j < intStudentCount - i - 1; j++)
+                {
+                    if (intGrade[j].CompareTo(intGrade[j + 1]) < 0)
+                    {
+                        tempGrade = intGrade[j];
+                        intGrade[j] = intGrade[j + 1];
+                        intGrade[j + 1] = tempGrade;
+
+                        tempFirstName = strFirstName[j];
+                        strFirstName[j] = strFirstName[j + 1];
+                        strFirstName[j + 1] = tempFirstName;
+
+                        tempLastName = strLastName[j];
+                        strLastName[j] = strLastName[j + 1];
+                        strLastName[j + 1] = tempLastName;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nHighest Grade:");
+            Console.WriteLine("Last Name\tFirst Name\tGrade");
+            Console.WriteLine(strLastName[0] + "\t\t" + strFirstName[0] + "\t\t" + intGrade[0]);
+
+            Console.WriteLine("\n Press Enter To Continue");
         }
         public static void GradeLow()
         {
+            string tempFirstName;
+            string tempLastName;
+            int tempGrade;
+
+            for (int i = 0; i < intStudentCount - 1; i++)
+            {
+                for (int j = 0; j < intStudentCount - i - 1; j++)
+                {
+                    if (intGrade[j].CompareTo(intGrade[j + 1]) > 0)
+                    {
+                        tempGrade = intGrade[j];
+                        intGrade[j] = intGrade[j + 1];
+                        intGrade[j + 1] = tempGrade;
+
+                        tempFirstName = strFirstName[j];
+                        strFirstName[j] = strFirstName[j + 1];
+                        strFirstName[j + 1] = tempFirstName;
+
+                        tempLastName = strLastName[j];
+                        strLastName[j] = strLastName[j + 1];
+                        strLastName[j + 1] = tempLastName;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nLowest Grade:");
+            Console.WriteLine("Last Name\tFirst Name\tGrade");
+            Console.WriteLine(strLastName[0] + "\t\t" + strFirstName[0] + "\t\t" + intGrade[0]);
+
+            Console.WriteLine("\n Press Enter To Continue");
         }
         public static void Median()
         {
+            string tempFirstName;
+            string tempLastName;
+            int tempGrade;
+            int intMid = intStudentCount/2;
+
+            for (int i = 0; i < intStudentCount - 1; i++)
+            {
+                for (int j = 0; j < intStudentCount - i - 1; j++)
+                {
+                    if (intGrade[j].CompareTo(intGrade[j + 1]) > 0)
+                    {
+                        tempGrade = intGrade[j];
+                        intGrade[j] = intGrade[j + 1];
+                        intGrade[j + 1] = tempGrade;
+
+                        tempFirstName = strFirstName[j];
+                        strFirstName[j] = strFirstName[j + 1];
+                        strFirstName[j + 1] = tempFirstName;
+
+                        tempLastName = strLastName[j];
+                        strLastName[j] = strLastName[j + 1];
+                        strLastName[j + 1] = tempLastName;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nMedian Grade:");
+            Console.WriteLine("Last Name\tFirst Name\tGrade");
+            Console.WriteLine(strLastName[intMid] + "\t\t" + strFirstName[intMid] + "\t\t" + intGrade[intMid]);
+
+            Console.WriteLine("\n Press Enter To Continue");
         }
     }
 }
